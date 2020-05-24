844abecdabbcdbbd9a670caabfc225122c1d5a83
Ritusmoi Kaushik <ritusmoikaushik@gmail.com>
2020-05-24 14:09:36 +0530

    Initial Mappings Initial Commit

---
+++ initial-mappings/src/main/java/com/codeinitializr/springbootbasics/initialmappings/InitialMappingsApplication.java
@@ -0,0 +1,13 @@
+package com.codeinitializr.springbootbasics.initialmappings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialMappingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitialMappingsApplication.class, args);
+	}
+
+}
---
+++ initial-mappings/src/test/java/com/codeinitializr/springbootbasics/initialmappings/InitialMappingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeinitializr.springbootbasics.initialmappings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitialMappingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
